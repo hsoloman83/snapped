3838dc92f17273500ad0afe0eadb39642e37865e
github username <github email address>
2018-07-09 10:20:33 +0100

    define relationships and bcrypt gem file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :pictures
+  has_many :comments
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true
+  validates :email, presence: true
+  has_secure_password
 end

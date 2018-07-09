4de6e28bdf283ac11d220cff208e31ecafd494e7
github username <github email address>
2018-07-09 10:05:38 +0100

    created tables and migrated

---
+++ db/migrate/20180709090037_create_pictures.rb
@@ -0,0 +1,11 @@
+class CreatePictures < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pictures do |t|
+      t.string :title
+      t.string :image_url
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
